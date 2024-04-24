9ff4a15e1e2279255c8e0c68dc7311dcd0453889
kelyan danis <kelyan.danis@ynov.com>
2024-04-24 10:21:40 +0200

    ✨ i add the conection menu ✨

--- myapp/public/javascripts/index.js
+++ myapp/public/javascripts/index.js
@@ -1,3 +1,14 @@
-import { io } from "https://cdn.socket.io/4.7.4/socket.io.esm.min.js";
-
-window.socket = io();
+//import { io } from "https://cdn.socket.io/4.7.4/socket.io.esm.min.js";
+//window.socket = io();
+document.getElementById("go").onclick = function() {
+    var name = document.forms["form1"]["Name"].value
+    var newRommID = document.forms["form2"]["roomID"].value
+    if (newRommID === '') {
+        alert('Empty Room ID !')
+    } else if (name === '') {
+        alert('Empty Name !')
+    } else {
+        document.cookie = 'roomID='+newRommID;
+        document.cookie = 'Name='+name;
+    };
+};
